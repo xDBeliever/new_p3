64e3e797227576da110c5afc70bb5191fb3cc5eb
xDAnon <161009089+xDAnon@users.noreply.github.com>
2024-02-24 15:36:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1392566136)
+ADMINS.append(6328168464)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
