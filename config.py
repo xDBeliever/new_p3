2d8d639a0715f408f507df93abedb57e571f8838
xDAnon <161009089+xDAnon@users.noreply.github.com>
2024-02-24 13:31:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://ChikenDb:xJJunLHz6ZzAruqe@cluster0.usbia1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "394197fedbb84bebba9b925a0bd2484a2c33d956")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "http://Instantearn.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "5b4af2fa7702ac014be08b5db557a2beac193563")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","leakhubd/1264")
